9351306fb5b2380bdf18ef5f544bb6fffd2b824e
Rian Xavier <rianalvesxvr@gmail.com>
2025-07-19 14:02:59 -0300

    feat: add missingLetter to GET response

--- src/modules/products/viewModel/ProductViewModel.ts
+++ src/modules/products/viewModel/ProductViewModel.ts
@@ -1,5 +1,19 @@
 import { Product } from '../entities/Product';
 
+const getMissingLetter = (productName: string) => {
+  const az = 'abcdefghijklmnopqrstuvwxyz';
+  const setLetters = new Set(
+    productName.toLocaleLowerCase().replace(/[^a-z]/g, ''),
+  );
+
+  for (const letter of az) {
+    if (!setLetters.has(letter)) {
+      return letter;
+    }
+  }
+
+  return '_';
+};
 export class ProductViewModel {
   static toHttp({ createdAt, name, price, sku, id }: Product) {
     return {
@@ -8,6 +22,7 @@ export class ProductViewModel {
       price,
       sku,
       createdAt,
+      missingLetter: getMissingLetter(name),
     };
   }
 }
